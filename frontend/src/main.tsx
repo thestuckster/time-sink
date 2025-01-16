964b5072e71f5b0d3f3673e8c572c27d32165ff3
Stephen Stucky <sstucky3@gmail.com>
2025-01-16 00:02:06 -0600

    Material UI (#1)
    💄 boilerplate in materialUI instead

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,12 +2,19 @@ import React from 'react'
 import {createRoot} from 'react-dom/client'
 import App from './App'
 
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+import {BrowserRouter, Route, Routes} from "react-router";
+
+
 const container = document.getElementById('root')
 
 const root = createRoot(container!)
 
 root.render(
-    <React.StrictMode>
-        <App/>
-    </React.StrictMode>
+  <React.StrictMode>
+    <App/>
+  </React.StrictMode>
 )
